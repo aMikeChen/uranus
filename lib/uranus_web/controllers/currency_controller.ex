ae905753d3b42c4c07c1f18a9245ae4fe387cef4
Mike Chen <mike.chen@access-company.com>
2021-07-20 15:08:10 +0800

    Add cache

--- lib/uranus_web/controllers/currency_controller.ex
+++ lib/uranus_web/controllers/currency_controller.ex
@@ -1,9 +1,26 @@
 defmodule UranusWeb.CurrencyController do
   use UranusWeb, :controller
 
+  alias Uranus.{Cache, Coin}
+
   def show(conn, %{"pair" => pair}) do
     [symbol, convert] = String.split(pair, "-")
-    price = Uranus.Coin.latest_price(symbol, convert)
+    price = latest_price(symbol, convert)
+
     render(conn, "show.json", %{price: price})
+  end
+
+  defp latest_price(symbol, convert) do
+    key = symbol <> convert
+
+    case Cache.Redis.get_value(key) do
+      nil ->
+        price = Coin.latest_price(symbol, convert)
+        Cache.Redis.set_value(key, price, expiry: 10)
+        price
+
+      price ->
+        String.to_float(price)
+    end
   end
 end
