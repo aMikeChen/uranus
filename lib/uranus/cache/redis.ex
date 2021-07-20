324d15cb78db3784fbafea6a5b591b52a8f83b7c
Mike Chen <mike.chen@access-company.com>
2021-07-20 14:54:17 +0800

    Refine redis lib

---
+++ lib/uranus/cache/redis.ex
@@ -0,0 +1,24 @@
+defmodule Uranus.Cache.Redis do
+  @behaviour Uranus.Cache
+
+  @impl Uranus.Cache
+  def get_value(key) do
+    Redix.command(:redix, ["GET", key])
+    |> elem(1)
+  end
+
+  def set_value(key, value, opt \\ []) do
+    expiry = Keyword.get(opt, :expiry)
+
+    parameters =
+      []
+      |> add_parameter(:set, key, value)
+      |> add_parameter(:expiry, expiry)
+
+    Redix.command(:redix, parameters)
+  end
+
+  defp add_parameter(parameters, :set, key, value), do: parameters ++ ["SET", key, value]
+  defp add_parameter(parameters, :expiry, nil), do: parameters
+  defp add_parameter(parameters, :expiry, value), do: parameters ++ ["EX", value]
+end
